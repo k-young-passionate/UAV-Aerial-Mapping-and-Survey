e9a4e5d69efdf94c7f481ba41d4e138ff814a6fe
choeyujin <barbby20@naver.com>
2018-10-12 18:18:30 -0400

    digit detection

---
+++ DroneMosaic/digit.py
@@ -0,0 +1,20 @@
+# import the necessary packages
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
+import cv2
+ 
+# define the dictionary of digit segments so we can identify
+# each digit on the thermostat
+DIGITS_LOOKUP = {
+	(1, 1, 1, 0, 1, 1, 1): 0,
+	(0, 0, 1, 0, 0, 1, 0): 1,
+	(1, 0, 1, 1, 1, 1, 0): 2,
+	(1, 0, 1, 1, 0, 1, 1): 3,
+	(0, 1, 1, 1, 0, 1, 0): 4,
+	(1, 1, 0, 1, 0, 1, 1): 5,
+	(1, 1, 0, 1, 1, 1, 1): 6,
+	(1, 0, 1, 0, 0, 1, 0): 7,
+	(1, 1, 1, 1, 1, 1, 1): 8,
+	(1, 1, 1, 1, 0, 1, 1): 9
+}
