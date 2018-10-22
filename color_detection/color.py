039c10a6d62a6a99bd7bf3fbd4d7bddeed685472
lufovic77 <lufovic77@gmail.com>
2018-10-22 17:51:15 -0400

    whole code

--- color_detection/color.py
+++ color_detection/color.py
@@ -19,29 +19,6 @@ boundaries = [
 	([103, 86, 65], [145, 133, 128])
 ]
 
-nCV and Python Color DetectionPython
-
-# import the necessary packages
-import numpy as np
-import argparse
-import cv2
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
-args = vars(ap.parse_args())
-
-# load the image
-image = cv2.imread(args["image"])
-
-# define the list of boundaries
-boundaries = [
-	([17, 15, 100], [50, 56, 200]),
-	([86, 31, 4], [220, 88, 50]),
-	([25, 146, 190], [62, 174, 250]),
-	([103, 86, 65], [145, 133, 128])
-]
-
 # loop over the boundaries
 for (lower, upper) in boundaries:
 	# create NumPy arrays from the boundaries
